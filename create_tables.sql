cd86b9074365b9be6a0303e720e3b14cd6cf0018
kwilson <kolbywilson63@gmail.com>
2023-10-03 19:29:44 -0500

    test

--- create_tables.sql
+++ create_tables.sql
@@ -1,27 +1,61 @@
--- Create the table for bitcoin - the table schema is the same
--- for each coin, but has different values for the columns
-CREATE TABLE bitcoin (
-	Unix_Timestamp INT,
-	Entry_Date TIMESTAMP WITH TIME ZONE,
-	Symbol VARCHAR(10),
-	Open_Price DECIMAL,
-	High_Price DECIMAL,
-	Low_Price DECIMAL,
-	Close_Price DECIMAL,
-	Coin_Volume DECIMAL,
-	PRIMARY KEY (Unix_Timestamp, Symbol)
-);
+-- Active: 1696137771867@@127.0.0.1@5432@crypto
 
--- Create the table for ethereum - the table schema is the same
+-- Create the table for bitcoin - the table schema is the same
+
 -- for each coin, but has different values for the columns
-CREATE TABLE ethereum (
-	Unix_Timestamp INT,
-	Entry_Date TIMESTAMP WITH TIME ZONE,
-	Symbol VARCHAR(10),
-	Open_Price DECIMAL,
-	High_Price DECIMAL,
-	Low_Price DECIMAL,
-	Close_Price DECIMAL,
-	Coin_Volume DECIMAL,
-	PRIMARY KEY (Unix_Timestamp, Symbol)
-);
+
+CREATE TABLE
+    bitcoin (
+        Unix_Timestamp INT,
+        Entry_Date TIMESTAMP
+        WITH
+            TIME ZONE,
+            Symbol VARCHAR(10),
+            Open_Price DECIMAL,
+            High_Price DECIMAL,
+            Low_Price DECIMAL,
+            Close_Price DECIMAL,
+            Coin_Volume DECIMAL,
+            PRIMARY KEY (Unix_Timestamp, Symbol)
+    );
+
+-- Create the table for kraken_bitcoin  - the table schema is the same
+
+-- for each coin, but has different values for the columns
+
+CREATE TABLE
+    kraken_bitcoin (
+        Unix_Timestamp INT,
+        Entry_Date TIMESTAMP
+        WITH
+            TIME ZONE,
+            Symbol VARCHAR(10),
+            Open_Price DECIMAL,
+            High_Price DECIMAL,
+            Low_Price DECIMAL,
+            Close_Price DECIMAL,
+            Coin_Volume DECIMAL,
+            Trades INT,
+            PRIMARY KEY (Unix_Timestamp, Symbol)
+    );
+
+-- Create the table for kraken_trades  - the table schema is the same
+
+-- for each coin, but has different values for the columns
+
+CREATE TABLE
+    kraken_trades (
+        Unix_Timestamp INT,
+        Entry_Date TIMESTAMP
+        WITH
+            TIME ZONE,
+            Symbol VARCHAR(10),
+            Price DECIMAL,
+            Volume DECIMAL,
+            PRIMARY KEY (
+                Unix_Timestamp,
+                Symbol,
+                Price,
+                Volume
+            )
+    );
